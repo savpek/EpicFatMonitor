3868f43fc31f05d7af3bac30b892eda46d851103
savpek <savolainen.pekka@gmail.com>
2013-08-04 18:41:56 +0300

    Your stats now calculates values correctly.

--- EpicFatMonitor/Scripts/App/controllers.js
+++ EpicFatMonitor/Scripts/App/controllers.js
@@ -3,15 +3,40 @@
 }
 
 fatMonitorApp.controller('HomeController', function ($scope, userService) {
-    var dailyWeightApi = userService;
+    if (userService.measurements !== undefined) {
+        $scope.weight = userService.measurements[0].Weight;
+        $scope.email = userService.email;
+    }
+
+    $scope.$on('userServiceUpdated', function () {
+        $scope.weight = userService.measurements[0].Weight;
+        $scope.email = userService.email;
+    });
 
     $scope.sendWeight = function () {
-        dailyWeightApi.get({}, function (user) {
-            $scope.weight = user.Measurements[0].Weight;
-        });
+        
     };
 });
 
-fatMonitorApp.controller('StatsController', function ($scope) {
-    $scope.items = ['foo', 'bar', 'jorma'];
-})
+fatMonitorApp.controller('StatsController', function ($scope, userService) {
+    var calculateValues = function () {
+        $scope.measurements = userService.measurements.sort(function (a, b) { return a.Time < b.Time; });
+        $scope.email = userService.email;
+
+        var initialWeight = $scope.measurements[0].Weight;
+
+        var i;
+        for (i = 0; i < $scope.measurements.length; i++) {
+            var weight = $scope.measurements[i].Weight;
+            $scope.measurements[i].fromInitialPercent = (weight / initialWeight * 100) - 100;
+        }
+    };
+
+    if (userService.email !== undefined) {
+        calculateValues();
+    }
+
+    $scope.$on('userServiceUpdated', function () {
+        calculateValues();
+    });
+});
---
+++ EpicFatMonitor/Scripts/App/services.js
@@ -0,0 +1,31 @@
+﻿var factories = {};
+
+factories.userService = function ($resource, $rootScope) {
+    var restApi = $resource('/rest/user/', {}, {
+        get: { method: "GET" },
+        update: { method: "PUT" },
+        isArray: false
+    });
+
+    var service = {};
+
+    restApi.get({}, function (user) {
+        console.log("User rest request completed.");
+        service.measurements = user.Measurements;
+        service.email = user.Email;
+
+        $rootScope.$broadcast('userServiceUpdated');
+    });
+
+    service.Save = function() {
+        console.log("service.Save called.");
+    };
+
+    service.ValueUpdated = function() {
+
+    };
+
+    return service;
+};
+
+fatMonitorApp.factory(factories);
