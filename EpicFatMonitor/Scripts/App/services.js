d086f31836ae6619ff7e915ea48931f6eab24071
savpek <savolainen.pekka@gmail.com>
2013-08-04 18:41:56 +0300

    Your stats now calculates values correctly.

---
+++ EpicFatMonitor/Scripts/App/services.js
@@ -0,0 +1,27 @@
+﻿var factories = {};
+
+factories.userService = function ($resource, $rootScope) {
+    var restApi = $resource('/rest/user/', {}, {
+        get: { method: "GET" },
+        update: { method: "PUT" },
+        isArray: false
+    });
+
+    var service = {};
+
+    restApi.get({}, function (user) {
+        console.log("User rest request completed.");
+        service.measurements = user.Measurements;
+        service.email = user.Email;
+
+        $rootScope.$broadcast('userServiceUpdated');
+    });
+
+    service.Save = function() {
+        console.log("service.Save called.");
+    };
+
+    return service;
+};
+
+fatMonitorApp.factory(factories);
